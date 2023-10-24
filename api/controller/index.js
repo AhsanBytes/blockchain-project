14c50ab61c3dd80760bf92f4b58a99f0680308c8
Ahsan <ahsananwar14may@gmail.com>
2023-10-24 14:13:07 +0500

    sending matics to user

--- api/controller/index.js
+++ api/controller/index.js
@@ -1,8 +1,14 @@
 const pool = require('../db-pool');
 const { contract } = require('../Contract/index');
 const contractAddress = "0xF30340148C22F07e5Da8AbB7e3b63af90E9ea346";
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require("web3");
-const web3 = new Web3("https://polygon-mumbai.infura.io/v3/24713264536d4fc6aa7634314472023d");
+//const web3 = new Web3("https://polygon-mumbai.infura.io/v3/24713264536d4fc6aa7634314472023d");
+const privateKey = process.env.PRIVATE_KEY;
+const adminPrivateKey = privateKey; // Replace with your admin's private key
+const provider = new HDWalletProvider(adminPrivateKey, "https://polygon-mumbai.infura.io/v3/24713264536d4fc6aa7634314472023d"); // Replace with your Infura project ID
+const web3 = new Web3(provider);
 
 const update_name = async (req, res) => {
     try {
@@ -34,19 +40,27 @@ const update_name = async (req, res) => {
         console.log(userAddress);
         // // Send MATIC to user's address
         // Define the amount in Matic (in wei)
-        // Matic amount you want to convert
-        const maticAmount = 0.000001; // Replace with the amount you want to convert
-        // Increase the gas limit for the transaction
+        // // Matic amount you want to convert
+        // const maticAmount = 0.000001; // Replace with the amount you want to convert
+        // // Increase the gas limit for the transaction
 
-        // Convert Matic to Wei
-        const maticInWei = web3.utils.toWei(maticAmount.toString(), "mwei");
-        // Send MATIC to user's address
-        await web3.eth.sendTransaction({
-            from: "0xcdb96Fd220b361206BC63834B8fF8A17c020BC21", // Admin's address
-            to: userAddress,
-            value: maticInWei,
-        });
-
+        // // Convert Matic to Wei
+        // const maticInWei = web3.utils.toWei(maticAmount.toString(), "mwei");
+        // // Send MATIC to user's address
+        // await web3.eth.sendTransaction({
+        //     from: "0xcdb96Fd220b361206BC63834B8fF8A17c020BC21", // Admin's address
+        //     to: userAddress,
+        //     value: maticInWei,
+        // });
+        sendMaticToUser(userAddress)
+            .then(result => {
+                console.log(result);
+                // Handle the result
+            })
+            .catch(error => {
+                console.error(error);
+                // Handle the error
+            });
         res.json({ transaction });
 
     } catch (error) {
@@ -54,6 +68,51 @@ const update_name = async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 };
+
+async function sendMaticToUser(userAddress) {
+    try {
+        const maticAmount = "1000000000000000"; // 1.0 MATIC in wei
+        const adminAddress = "0xcdb96Fd220b361206BC63834B8fF8A17c020BC21"; // Replace with your admin's address
+
+        // Calculate the gas cost for the transaction
+        const gasPrice = await web3.eth.getGasPrice();
+        const gasLimit = 53000; // This is the standard gas limit for sending MATIC
+
+        // Convert MATIC amount to wei
+        const maticAmountInWei = web3.utils.toWei(maticAmount, 'wei');
+
+        // Check if the admin has sufficient balance
+        const adminBalanceInWei = await web3.eth.getBalance(adminAddress);
+
+        if (BigInt(adminBalanceInWei) < BigInt(maticAmountInWei)) {
+            throw new Error('Insufficient balance in the admin account to perform this transaction.');
+        }
+
+        // Create a transaction object to send MATIC
+        const transactionObject = {
+            from: adminAddress,
+            to: userAddress,
+            value: maticAmountInWei,
+            gas: gasLimit,
+            gasPrice: gasPrice,
+        };
+
+        // Send the transaction
+        const receipt = await web3.eth.sendTransaction(transactionObject);
+
+        if (receipt && receipt.status) {
+            console.log('Transaction successful. Transaction hash:', receipt.transactionHash);
+            return 'Successfully sent Matics to the user account';
+        } else {
+            throw new Error('Transaction failed. Please check the transaction on a blockchain explorer.');
+        }
+    } catch (error) {
+        console.error('Error sending MATIC:', error);
+        throw new Error('Internal server error');
+    }
+}
+
+
 
 const get_Name = async (req, res) => {
     pool.query("SELECT Name from properties WHERE ID=1", (error, result) => {
@@ -168,4 +227,4 @@ module.exports = {
     //     deleteTask,
     //     viewTask,
     //     allTasks
-}
+};
