21986e97daa299b59a1c296a1bb27131bbdb2bbc
Ahsan <ahsananwar14may@gmail.com>
2023-10-29 14:08:34 +0500

    new

--- api/Contract/index.js
+++ api/Contract/index.js
@@ -6,4 +6,4 @@ const web3 = new Web3("https://polygon-mumbai.infura.io/v3/24713264536d4fc6aa763
 const contractAddress = "0xF30340148C22F07e5Da8AbB7e3b63af90E9ea346";
 const contract = new web3.eth.Contract(ABI, contractAddress);
 
-module.exports={contract, web3}
+module.exports={contract, web3};
